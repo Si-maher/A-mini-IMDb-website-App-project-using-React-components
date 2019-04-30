e2ee5f4baf0f6ba29a6d5e59fdfd8c154d5f511d
Simon Maher <simon.maher@integrify.io>
2019-04-30 16:44:50 +0300

    created header image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-ReactDOM.render(<p>START HERE</p>, document.getElementById('root'));
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App/App"
+ReactDOM.render(<App />, document.getElementById("root"))
