e52505f6eb244c8ff1ad2bef24805127957b27c8
Simon Maher <simon.maher@integrify.io>
2019-05-03 16:58:22 +0300

    added home components, movie and title components

---
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Fontawesome from "react-fontawesome"
+import { calcTime, convertMoney } from "../../../helpers"
+import "./MovieInfoBar.css"
+
+const MovieInfoBar = (props) => {
+    return (
+        <div>
+            MovieInfoBar
+        </div>
+    )
+}
+export default MovieInfoBar
